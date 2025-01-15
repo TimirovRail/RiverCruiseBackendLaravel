e426dcc2120e3333840a496dab03766f24fcb438
Timirov Rail <timirov.rail@inbox.ru>
2025-01-15 23:51:25 +0300

    new changed v2

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Booking;
+
+class BookingController extends Controller
+{
+    public function index()
+    {
+        $bookings = Booking::all(); 
+        return response()->json($bookings);
+    }
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'cruise' => 'required|string|max:255',
+            'date' => 'required|date',
+            'seats' => 'required|integer|min:1|max:10',
+            'cabinClass' => 'required|string|max:255',
+            'extras' => 'nullable|array',
+            'extras.*' => 'string|max:255',
+            'comment' => 'nullable|string|max:1000',
+        ]);
+
+        $booking = Booking::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'cruise' => $validatedData['cruise'],
+            'date' => $validatedData['date'],
+            'seats' => $validatedData['seats'],
+            'cabin_class' => $validatedData['cabinClass'],
+            'extras' => json_encode($validatedData['extras']),
+            'comment' => $validatedData['comment'],
+        ]);
+
+        return response()->json(['message' => 'Booking successfully created!', 'data' => $booking], 201);
+    }
+}
